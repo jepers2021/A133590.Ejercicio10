c321bc7763388228e8fe0f07b235f9fda71182c4
Jorge <jepers2021@gmail.com>
2021-03-26 22:11:35 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A133590.Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 10");
+            Console.WriteLine("Dada una frase por el usuario, el programa presenta la misma frase en minúscula.");
+            Console.Write("Por favor, ingrese una frase: ");
+            string frase = Console.ReadLine();
+            Console.WriteLine(frase.ToLowerInvariant());
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
